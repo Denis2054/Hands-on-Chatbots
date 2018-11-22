9cf533333247829c1a269b519f9342a2930fde26
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2018-11-22 10:12:38 +0100

    Add files via upload

---
+++ HOC/controllers/helpers/processMessage.js
@@ -0,0 +1,27 @@
+const API_AI_TOKEN = "<YOUR API TOKEN>";
+const apiAiClient = require("<API AI TOKEN>")(API_AI_TOKEN);
+const FACEBOOK_ACCESS_TOKEN = "<YOUR FACEBOOK_ACESS_TOKEN";
+const request = require("request");
+const sendTextMessage = (senderId, text) => {
+ request({
+ url: "https://graph.facebook.com/v2.6/me/messages",
+ qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+ method: "POST",
+ json: {
+ recipient: { id: senderId },
+ message: { text },
+ }
+ });
+};
+module.exports = (event) => {
+ console.log("export");
+ const senderId = event.sender.id;
+ const message = event.message.text;
+const apiaiSession = apiAiClient.textRequest(message, {sessionId: "YOUR VERIFICATION TOKEN"});
+apiaiSession.on("response", (response) => {
+ const result = response.result.fulfillment.speech;
+sendTextMessage(senderId, result);
+ });
+apiaiSession.on("error", error => console.log(error));
+ apiaiSession.end();
+};
---
+++ HOC/controllers/verification.js
@@ -0,0 +1,11 @@
+module.exports = (req, res) => {
+    const hubChallenge = req.query["hub.challenge"];
+    const hubMode = req.query["hub.mode"];
+    const verifyTokenMatches = (req.query["hub.verify_token"] === "cpcs");
+
+    if (hubMode && verifyTokenMatches) {
+        res.status(200).send(hubChallenge);
+    } else {
+        res.status(403).end();
+    }
+};
